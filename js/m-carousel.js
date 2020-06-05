f5905a85dbb3ff514f95cabf545b07903c872854
Ira <moby.dick.killer@gmail.com>
2020-06-05 17:38:36 +0300

    first commit

---
+++ js/m-carousel.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        autoWidth:true,
+        items:2,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplaySpeed:2000
+    });
+});
